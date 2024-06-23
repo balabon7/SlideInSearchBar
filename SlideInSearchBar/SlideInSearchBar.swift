d7caabbacc013c483bfab3789d5c65d0cfe3a66d
balabon7 <balabon7@gmail.com>
2024-06-24 01:16:28 +0300

    Created a test example of a sliding placeholder in SearchBar

---
+++ SlideInSearchBar/SlideInSearchBar.swift
@@ -0,0 +1,8 @@
+//
+//  SlideInSearchBar.swift
+//  SlideInSearchBar
+//
+//  Created by Oleksandr Balabon on 24.06.2024.
+//
+
+import Foundation
---
+++ SlideInSearchBar/TestingSearchBarViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TestingSearchBarViewController.swift
+//  SlideInSearchBar
+//
+//  Created by Oleksandr Balabon on 24.06.2024.
+//
+
+import Foundation
